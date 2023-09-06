0f4f5eef6aa515bfa438356b8ce96233d15593c3
herusetiajaya <setyaone27@gmail.com>
2023-09-06 18:17:37 +0700

    pagination

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,8 +2,7 @@
 
 @section('container')
     <h1 class="mb-3">{{ $title }}</h1>
-
-    <div class="row justify-content-center">
+    <div class="row justify-content-center mb-3">
         <div class="col-md-6">
             <form action="/blog">
                 @if (request('category'))
@@ -67,5 +66,9 @@
     @else
         <p class="text-center fs-4">No post found.</p>
     @endif
+
+    <div class="d-flex justify-content-center">
+        {{ $posts->links() }}
+    </div>
 
 @endsection
