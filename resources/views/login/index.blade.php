e52183a82d9350373e5887c7e5a78d9b22b783af
herusetiajaya <setyaone27@gmail.com>
2023-09-07 16:39:59 +0700

    user registration

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -5,10 +5,16 @@
 <div class="row justify-content-center">
     <div class="col-md-4">
         <main class="form-signin bg-light p-3 mb-3 mt-3 rounded-3">
-            <img class="mb-2 rounded-3" src="https://source.unsplash.com/500x200?logo" alt="" width="72" height="57">
+          @if(session()->has('success'))
+          <div class="alert alert-info alert-dismissible fade show" role="alert">
+            <strong>{{ session('success') }}</strong> Please login
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+          @else
+          <img class="mb-2 rounded-3" src="https://source.unsplash.com/500x200?water" alt="" width="72" height="57">
+          @endif
             <h1 class="h3 mb-3 fw-normal">Please Login</h1>
             <form>
-
               <div class="form-floating">
                 <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
                 <label for="floatingInput">Email address</label>
@@ -17,7 +23,6 @@
                 <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
                 <label for="floatingPassword">Password</label>
               </div>
-
               <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
             </form>
             <small>
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -5,30 +5,46 @@
 <div class="row justify-content-center">
     <div class="col-lg-4">
         <main class="form-registration bg-light p-3 mb-3 mt-3 rounded-3">
-            <img class="mb-2 rounded-3" src="https://source.unsplash.com/500x200?logo" alt="" width="72" height="57">
+            <img class="mb-2 rounded-3" src="https://source.unsplash.com/500x200?water" alt="" width="72" height="57">
             <h1 class="h3 mb-3 fw-normal">Registration Form</h1>
-            <form>
-
+            <form action="/register" method="post">
+                @csrf
             <div class="form-floating">
-                <input type="text" class="form-control rounded-top" name="name" id="name" placeholder="Name">
+                <input type="text" class="form-control rounded-top @error('name') is-invalid @enderror" name="name" id="name" placeholder="Name" required value="{{ old('name') }}">
                 <label for="floatingInput">Name</label>
+                @error('name')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+                @enderror
             </div>
-
             <div class="form-floating">
-                <input type="text" class="form-control" name="username" id="username" placeholder="Username">
+                <input type="text" class="form-control @error('username') is-invalid @enderror" name="username" id="username" placeholder="Username" required value="{{ old('username') }}">
                 <label for="floatingInput">Username</label>
+                @error('username')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+                @enderror
             </div>
-
             <div class="form-floating">
-                <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com">
+                <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" placeholder="name@example.com" required value="{{ old('email') }}">
                 <label for="floatingInput">Email address</label>
+                @error('email')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+                @enderror
             </div>
-
             <div class="form-floating">
-                <input type="password" class="form-control rounded-bottom" name="password" id="password" placeholder="Password">
+                <input type="password" class="form-control rounded-bottom @error('password') is-invalid @enderror" name="password" id="password" placeholder="Password" required>
                 <label for="floatingPassword">Password</label>
+                @error('password')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+                @enderror
             </div>
-
             <button class="w-100 btn btn-lg btn-primary mt-3" type="submit">Register</button>
             </form>
             <small>
