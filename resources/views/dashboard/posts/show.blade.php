fcef7d574084d2c32ec124e4938b4c10d6e25115
herusetiajaya <setyaone27@gmail.com>
2023-09-09 06:57:00 +0700

    update and delete post

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -6,8 +6,12 @@
     <div class="row my-3">
         <div class="col-lg-8">
             <a href="/dashboard/posts" class="badge bg-success"><span data-feather="arrow-left"></span></a>
-            <a href="" class="badge bg-warning"><span data-feather="edit"></span></a>
-            <a href="" class="badge bg-danger"><span data-feather="x-circle"></span></a>
+            <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
+            <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                @method('delete')
+                @csrf
+                <button class="badge bg-danger border-0" onclick="return confirm('Are you sure?')"><span data-feather="x-circle"></span></button>
+            </form>
             <div class="card my-3 p-3">
                 <h4>{{ $post->title }}</h4>
                 By : {{ $post->author->name }}, Category {{ $post->category->name }}
